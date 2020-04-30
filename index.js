c2b9c2e7ff5991c795fd781f589575bcb9ff6367
Rubén <hey@rubenvara.io>
2020-04-30 13:37:26 +0200

    index 🏡

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // export the calculator fn
 module.exports = require('./lib/correlation');
 
-// TODO maybe export and obj of other stats like avg, std dev, variance, etc... 🤔
+// TODO maybe provide name exports of other functions for avg, std dev, variance, etc... 🤔
