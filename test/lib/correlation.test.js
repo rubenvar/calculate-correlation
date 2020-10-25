98d6e4104c2571526107b8cf5d8aded02b35c5cc
Rubén <hey@rubenvara.io>
2020-10-25 16:51:40 +0100

    remove test for more than 2 args

--- test/lib/correlation.test.js
+++ test/lib/correlation.test.js
@@ -1,15 +1,14 @@
 const calculateCorrelation = require('../../lib/correlation');
 
 describe('calculateCorrelation function args', () => {
-  it('throws an error if more or less than two args', () => {
+  it('throws an error if less than two args', () => {
     // wrap the call in another fn to let the assertion invoke the fn
     expect(() => calculateCorrelation([1, 2, 3])).to.throw();
-    expect(() => calculateCorrelation([1, 2], [2, 1], [3, 2])).to.throw();
-    expect(() => calculateCorrelation(1, 2, 2, 1, 3, 2)).to.throw();
   });
   it('throws an error if args not of correct type of form', () => {
     expect(() => calculateCorrelation([1, 2], [3])).to.throw();
     expect(() => calculateCorrelation(['one', 'three'], [1, 3])).to.throw();
+    expect(() => calculateCorrelation(1, 2, 2, 1, 3, 2)).to.throw();
   });
 });
 
